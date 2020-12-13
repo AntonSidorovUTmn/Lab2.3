c22d1787c1578404ff9a03c2e0cf899a1617cdaa
Anon Sidorov <73869696+AntonSidorovUTmn@users.noreply.github.com>
2020-12-13 21:27:26 +0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1 {
+
+    public partial class Form1: Form {
+        delegate double TheFunc(double x);
+        delegate IntPtr FuncName();
+
+        [DllImport("Lib2-2-1.dll", EntryPoint = "TheFunc", CallingConvention = CallingConvention.Cdecl)]
+        public static extern double TheFunc1(double x);
+
+        [DllImport("Lib2-2-2.dll", EntryPoint = "TheFunc", CallingConvention = CallingConvention.Cdecl)]
+        public static extern double TheFunc2(double x);
+
+        [DllImport("Lib2-2-3.dll", EntryPoint = "TheFunc", CallingConvention = CallingConvention.Cdecl)]
+        public static extern double TheFunc3(double x);
+
+        [DllImport("MyCppDll.dll", EntryPoint = "TheFunc", CallingConvention = CallingConvention.Cdecl)]
+        public static extern double TheFunc4(double x);
+
+        [DllImport("MyCDll.dll", EntryPoint = "TheFunc", CallingConvention = CallingConvention.Cdecl)]
+        public static extern double TheFunc5(double x, double a, double b, double c);
+
+        [DllImport("project1.dll", EntryPoint = "TheFunc", CallingConvention = CallingConvention.Cdecl)]
+        public static extern double TheFunc6(double x);
+
+        public Form1(int index, string FuncName) {
+            InitializeComponent();
+            TheFunc func;
+            switch (index) {
+                case 0: func = TheFunc1; break;
+                case 1: func = TheFunc2; break;
+                case 2: func = TheFunc3; break;
+                case 3: func = TheFunc4; break;
+                case 4: func = TheFunc6; break;
+                default: func = TheFunc1; break;
+            }
+            double y;
+            for (double x = 0; x <= 10; x++) {
+                chart1.Series[0].Name = FuncName;
+                y = func(x);
+                chart1.Series[FuncName].Points.AddXY(x, y);
+            }
+        }
+
+        public Form1(string FuncName, double a, double b, double c) {
+            InitializeComponent();
+            double y;
+            for (double x = 0; x <= 10; x++) {
+                chart1.Series[0].Name = FuncName;
+                y = TheFunc5(x, a, b, c);
+                chart1.Series[FuncName].Points.AddXY(x, y);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ *Лабораторная: 2.3.
+ *Источник: Hwmw.Blogspot.com
+ *
+ *Язык: C Sharp (C#) v7.3.
+ *Среда: Microsoft Visual Studio 2019 v16.7.6.
+ *Платформа: .NET Framework v4.7.2.
+ *API: console.
+ *Изменение: 28.11.2020.
+ *Защита: 14.12.2020.
+ *
+ *Задание: дополните папку из Лаб 2.2 новыми библиотеками написанными на C, C++ и Delphi (Lazarus). Проверьте их
+ *     рабочеспособность запустив программу из Lab 2.2.
+ */
+
+using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1 {
+
+    static class Program {
+        [STAThread]
+
+        static void Main () {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form2());
+        }
+    }
+}
